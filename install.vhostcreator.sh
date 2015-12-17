175ba2e1c1b987686264a362ee50089c4dffbe25
Matheus Fidelis <msfidelis@github.com>
2015-12-17 08:54:53 -0200

    add bin folder

---
+++ install.vhostcreator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "INSTALANDO O VHOST CREATOR"
+cp bin/vhost-creator /bin
+chmod 777 /bin/vhost-creator
+
+echo $"
+
+
+#################################################################################
+#			VHOST CREATOR SCRIPT - Debian Only9			#
+#	USAGE: vhost-creator -h you.virtualhost.com -d /path/to/project		#	
+#			    v1.0 - Matheus Fidelis				#
+#################################################################################
+"
