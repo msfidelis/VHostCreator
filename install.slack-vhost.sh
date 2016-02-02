a7d93c58386af7fac27f9ad77082fdf4424d9598
Matheus Fidelis <msfidelis@outlook.com>
2016-02-02 19:26:01 -0200

    Slackware Updates

--- install.slack-vhost.sh
+++ install.slack-vhost.sh
@@ -8,8 +8,12 @@ echo $"
 
 
 #################################################################################
-#			VHOST CREATOR SCRIPT - Slackware 14.1								#
-#	USAGE: slack-vhost -h you.virtualhost.com -d /path/to/project				#	
-#			    v1.0 - Matheus Fidelis											#
+			VHOST CREATOR SCRIPT - Slackware 14.1			
+	USAGE: slack-vhost -h you.virtualhost.com -d /path/to/project			
+			    v1.0 - Matheus Fidelis				
 #################################################################################
+
+OBS: TENHA CERTEZA DE TER DESCOMENTADO A LINHA 487 DO ARQUIVO /etc/httpd/httpd.conf
+
+Include /etc/httpd/extra/httpd-vhosts.conf :)
 "
