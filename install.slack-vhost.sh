4d5bc0ec55fdccaf8b6fd5cd048207a49b6e0c20
Matheus Fidelis <msfidelis@outlook.com>
2016-02-02 15:21:39 -0200

    Slackware Version

---
+++ install.slack-vhost.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "INSTALANDO O VHOST CREATOR"
+
+cp bin/slack-vhost /bin
+chmod 777 /bin/slack-vhost
+
+echo $"
+
+
+#################################################################################
+#			VHOST CREATOR SCRIPT - Slackware 14.1								#
+#	USAGE: slack-vhost -h you.virtualhost.com -d /path/to/project				#	
+#			    v1.0 - Matheus Fidelis											#
+#################################################################################
+"
