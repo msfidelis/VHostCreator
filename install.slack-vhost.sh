b67eb3aa91ad751a4a861616d8c200c203ac23ee
Matheus Scarpato Fidelis <matheus@fidelis.industriafox.com.br>
2016-02-05 08:54:44 -0200

    Validações de usuário para instalação e criação de hosts

--- install.slack-vhost.sh
+++ install.slack-vhost.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 echo "INSTALANDO O VHOST CREATOR"
+if [[ $EUID -ne 0 ]]; then
+	echo -e "Este Script deve ser executado como Root ou Sudo \n"
+	echo -e "\t\t\t Exemplo: sudo ./install.slack-vhost.sh  \n"
+exit 1
+else
+	cp bin/slack-vhost /bin
+	chmod 777 /bin/slack-vhost
 
-cp bin/slack-vhost /bin
-chmod 777 /bin/slack-vhost
-
-echo $"
+	echo $"
 
 
-#################################################################################
-			VHOST CREATOR SCRIPT - Slackware 14.1			
-	USAGE: slack-vhost -h you.virtualhost.com -d /path/to/project			
-			    v1.0 - Matheus Fidelis				
-#################################################################################
+	#################################################################################
+				VHOST CREATOR SCRIPT - Slackware 14.1
+		USAGE: slack-vhost -h you.virtualhost.com -d /path/to/project
+				    v1.0 - Matheus Fidelis
+	#################################################################################
 
-OBS: TENHA CERTEZA DE TER DESCOMENTADO A LINHA 487 DO ARQUIVO /etc/httpd/httpd.conf
+	OBS: TENHA CERTEZA DE TER DESCOMENTADO A LINHA 487 DO ARQUIVO /etc/httpd/httpd.conf
 
-Include /etc/httpd/extra/httpd-vhosts.conf :)
-"
+	Include /etc/httpd/extra/httpd-vhosts.conf :)
+	"
+fi
--- install.vhostcreator.sh
+++ install.vhostcreator.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 echo "INSTALANDO O VHOST CREATOR"
-cp bin/vhost-creator /bin
-chmod 777 /bin/vhost-creator
+if [[ $EUID -ne 0 ]]; then
+	echo -e "Este Script deve ser executado como Root ou Sudo \n"
+	echo -e "\t\t\t Exemplo: sudo ./install.vhostcreator.sh  \n"
+exit 1
+else
 
-echo $"
+  cp bin/vhost-creator /bin
+  chmod 777 /bin/vhost-creator
+
+  echo $"
 
 
-#################################################################################
-#			VHOST CREATOR SCRIPT - Debian Only9			#
-#	USAGE: vhost-creator -h you.virtualhost.com -d /path/to/project		#	
-#			    v1.0 - Matheus Fidelis				#
-#################################################################################
-"
+  #################################################################################
+  #			VHOST CREATOR SCRIPT - Debian Only9			#
+  #	USAGE: vhost-creator -h you.virtualhost.com -d /path/to/project		#
+  #			    v1.0 - Matheus Fidelis				#
+  #################################################################################
+  "
+fi
